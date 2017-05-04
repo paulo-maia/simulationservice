9b6f47689cf7800b638efdb511330caa84f90284
pvilaca <pvilaca@macbook>
2017-05-04 09:15:00 +0200

    [update] idtranslation

--- src/main/java/com/silicolife/dddecaf/rest/IdTranslation/IdTranslationRest.java
+++ src/main/java/com/silicolife/dddecaf/rest/IdTranslation/IdTranslationRest.java
@@ -1,5 +1,14 @@
 package com.silicolife.dddecaf.rest.IdTranslation;
 
-public class IdTranslationRest {
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.HttpRequestWithBody;
 
+public class IdTranslationRest {
+	
+	HttpRequestWithBody service;
+	
+	
+	public IdTranslationRest(String uri){
+		service = Unirest.post("http://localhost:8080/model/iJO1366/");
+	}
 }
